40817995a015a4304b83d5a5cec226b257ede6c7
Mikael Simberg <simberg@cscs.ch>
2025-10-03 16:57:22 +0200

    Progress after submitting pack kernels

--- include/ghex/packer.hpp
+++ include/ghex/packer.hpp
@@ -291,6 +291,9 @@ struct packer<gpu>
                 }
             }
         }
+
+        comm.progress();
+
         // await_futures(stream_futures, [&comm, &send_reqs](send_buffer_type* b)
         //     { send_reqs.push_back(comm.send(b->buffer, b->rank, b->tag)); });
         // await_futures(std::vector<Future>& range, Continuation&& cont)
