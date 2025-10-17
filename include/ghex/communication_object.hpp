c6fe7b372f7a35e5d55f7a776bc62ec48941ad3a
Mikael Simberg <simberg@cscs.ch>
2025-10-17 09:13:22 +0200

    never destroy nccl comm

--- include/ghex/communication_object.hpp
+++ include/ghex/communication_object.hpp
@@ -264,8 +264,11 @@ class communication_object
     }
     ~communication_object() noexcept {
       // TODO: nothrow
+      std::ostringstream msg_destroy;
+      msg_destroy << "~communication_object destroying nccl communicator\n";
+      std::cerr << msg_destroy.str();
       GHEX_CHECK_CUDA_RESULT_NO_THROW(cudaDeviceSynchronize());
-      GHEX_CHECK_NCCL_RESULT_NO_THROW(ncclCommDestroy(m_nccl_comm));
+      // GHEX_CHECK_NCCL_RESULT_NO_THROW(ncclCommDestroy(m_nccl_comm));
     }
     communication_object(const communication_object&) = delete;
     communication_object(communication_object&&) = default;
